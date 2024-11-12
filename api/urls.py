a5377c470c36b215abc05499f9a71e74e4eb4c48
Jorge Barrientos <elgigototo@hotmail.com>
2024-11-12 12:26:21 -0600

    adding conection to react project

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,17 @@
 # api/urls.py
 
 from django.urls import path
-from .views import TableroListCreate, TableroCRUD
+from .views import TableroListCreate, TableroCRUD, RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path("tableros/", TableroListCreate.as_view(), name="tablero-list-create"),
     path("tableros/<int:pk>", TableroCRUD.as_view(), name="tablero-detail"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
